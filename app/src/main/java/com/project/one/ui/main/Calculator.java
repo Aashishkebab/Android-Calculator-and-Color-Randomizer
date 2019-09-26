20a17fd91dc7ace1792952d2139e3a17f38d76db
Aashishkebab <aashish.bharadwaj01@gmail.com>
2019-09-26 01:48:52 -0400

    Some more cleanup

--- app/src/main/java/com/project/one/ui/main/Calculator.java
+++ app/src/main/java/com/project/one/ui/main/Calculator.java
@@ -19,19 +19,11 @@ import com.project.one.R;
  * create an instance of this fragment.
  */
 public class Calculator extends Fragment{
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     View view;
 
     enum Operation{
-        ADD, SUBTRACT, MULTIPLY, DIVIDE, NOTHING;
+        ADD, SUBTRACT, MULTIPLY, DIVIDE, NOTHING
     }
 
     private double buffer, memory;
@@ -64,10 +56,6 @@ public class Calculator extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        if(getArguments() != null){
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -221,8 +209,9 @@ public class Calculator extends Fragment{
         else{
             if(number % 1 > 0){
                 updateDisplay(Double.toString(number));
-            }else{
-                updateDisplay(Long.toString((long)number));
+            }
+            else{
+                updateDisplay(Long.toString((long) number));
             }
         }
 
